711003fb9eb14506b157ea57d2515ea4b6e1304d
diyapatel42 <65690319+diyapatel42@users.noreply.github.com>
2021-05-31 14:36:37 -0400

    Add files via upload

---
+++ R1.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : R1.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(void) {
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf(pow(5,3));
+	return 0;
+
+}
